440c1bfea4cec805c6e93348ecf2722f2813c06e
Benjamin Nortier <bjnortier@gmail.com>
2013-01-08 21:59:50 +0000

    Polygon from plane

--- test/spec/polygon.test.js
+++ test/spec/polygon.test.js
@@ -29,11 +29,54 @@ define(['lib/plane', 'lib/polygon'], function(Plane, Polygon) {
 
         });
 
-        it.skip('can be constructed from a plane', function() {
+        it('can be constructed from a plane', function() {
 
-            var h = new Plane(0,0,1,0);
-            var p1 = new Polygon().fromPlane(h);
+            var p1a = new Polygon().fromPlane(new Plane(5,-2,1,0));
+            var p1b = new Polygon().fromPlane(new Plane(-6,2,-1,0));
+            var p2a = new Polygon().fromPlane(new Plane(-6,7,2,0));
+            var p2b = new Polygon().fromPlane(new Plane(0,-8,3,0));
+            var p3a = new Polygon().fromPlane(new Plane(2,1,9,0));
+            var p3b = new Polygon().fromPlane(new Plane(0,1,-2,0));
+            var p4 = new Polygon().fromPlane(new Plane(1,1,1,0));
 
+            var xplus = {a:1,b:0,c:0,d:1000000},
+                yplus = {a:0,b:1,c:0,d:1000000},
+                zplus = {a:0,b:0,c:1,d:1000000},
+                xmin = {a:-1,b:0,c:0,d:1000000},
+                ymin = {a:0,b:-1,c:0,d:1000000},
+                zmin = {a:0,b:0,c:-1,d:1000000};
+
+            assert.deepEqual(p1a, {
+                s: {a:5, b:-2, c:1, d:0},
+                boundingPlanes: [yplus, zplus, ymin, zmin]
+            });
+            assert.deepEqual(p1b, {
+                s: {a:-6, b:2, c:-1, d:0},
+                boundingPlanes: [yplus, zmin, ymin, zplus]
+            });
+
+            assert.deepEqual(p2a, {
+                s: {a:-6, b:7, c:2, d:0},
+                boundingPlanes: [zplus, xplus, zmin, xmin]
+            });
+            assert.deepEqual(p2b, {
+                s: {a:0, b:-8, c:3, d:0},
+                boundingPlanes: [zplus, xmin, zmin, xplus]
+            });
+
+            assert.deepEqual(p3a, {
+                s: {a:2, b:1, c:9, d:0},
+                boundingPlanes: [xplus, yplus, xmin, ymin]
+            });
+            assert.deepEqual(p3b, {
+                s: {a:0, b:1, c:-2, d:0},
+                boundingPlanes: [xplus, ymin, xmin, yplus]
+            });
+
+            assert.deepEqual(p4, {
+                s: {a:1, b:1, c:1, d:0},
+                boundingPlanes: [yplus, zplus, ymin, zmin]
+            });
 
         })
 
