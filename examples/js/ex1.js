d4650171575e56ecfc84fe2472cb34fcc19528c7
Benjamin Nortier <bjnortier@gmail.com>
2013-01-10 08:19:42 +0000

    Front & back

--- examples/js/ex1.js
+++ examples/js/ex1.js
@@ -27,7 +27,7 @@ define(['examples/js/viewport', 'lib/plane', 'lib/polygon'], function(Viewport, 
     var addPolygon = function(viewport, polygon, color) {
         var geometry = polygonToMesh(polygon);
         var object = THREE.SceneUtils.createMultiMaterialObject(geometry, [
-            new THREE.MeshLambertMaterial({color: color, side: THREE.DoubleSide, ambient: color}),
+            new THREE.MeshLambertMaterial({color: color, side: THREE.DoubleSide, opacity: 0.5, transparent: true}),
             new THREE.MeshBasicMaterial({color: 0x000000, wireframe: true})
         ]);
         viewport.scene.add(object);
@@ -42,24 +42,33 @@ define(['examples/js/viewport', 'lib/plane', 'lib/polygon'], function(Viewport, 
         var exampleContainer = document.createElement('div');
         exampleContainer.classList.add('example');
         var beforeContainer = document.createElement('div');
-        var afterContainer = document.createElement('div');
+        var frontContainer = document.createElement('div');
+        var backContainer = document.createElement('div');
         beforeContainer.classList.add('viewport');
-        afterContainer.classList.add('viewport');
+        frontContainer.classList.add('viewport');
+        backContainer.classList.add('viewport');
         document.body.appendChild(exampleContainer);
         exampleContainer.appendChild(beforeContainer);
-        exampleContainer.appendChild(afterContainer);
+        exampleContainer.appendChild(frontContainer);
+        exampleContainer.appendChild(backContainer);
         var beforeViewport = new Viewport(beforeContainer);
-        var afterViewport  = new Viewport(afterContainer);
+        var frontViewport  = new Viewport(frontContainer);
+        var backViewport  = new Viewport(backContainer);
 
         var p1 = new Polygon(new Plane(0,0,1,0), [new Plane(1,0,0,-1), new Plane(0,1,0,-1), new Plane(1,1,0,5)]);
-        var h = new Plane(0,100000,0,0);
-        var p2 = p1.splitBy(h); 
+        var hplus = new Plane(0,1,0,0);
+        var hminus = new Plane(0,-1,0,0);
+        var pfront = p1.splitBy(hplus); 
+        var pback = p1.splitBy(hminus); 
         
         addPolygon(beforeViewport, p1, 0x00ff00);
-        addPlane(beforeViewport, h, 0xff0000);
+        addPlane(beforeViewport, hplus, 0xff0000);
 
-        addPolygon(afterViewport, p2, 0x0000ff);
-        addPlane(afterViewport, h, 0xff0000);
+        addPolygon(frontViewport, pfront, 0x0000ff);
+        addPlane(frontViewport, hplus, 0xff0000);
+
+        addPolygon(backViewport, pback, 0x0000ff);
+        addPlane(backViewport, hminus, 0xff0000);
     }
 
     return Example;
