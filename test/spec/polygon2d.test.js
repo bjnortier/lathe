4c76b901354fcf0acf3bf01fe743207a0a419c2f
Benjamin Nortier <bjnortier@gmail.com>
2013-01-22 21:54:27 +0000

    2D Polygon Split

--- test/spec/polygon2d.test.js
+++ test/spec/polygon2d.test.js
@@ -24,89 +24,39 @@ define(['lib/plane2d', 'lib/polygon2d'], function(Plane2D, Polygon2D) {
 
         });
 
-        // it('can be constructed from a plane', function() {
+        it('can be split by a plane', function() {
 
-        //     var p1a = new Polygon2D().fromPlane(new Plane2D(5,-2,1,0));
-        //     var p1b = new Polygon2D().fromPlane(new Plane2D(-6,2,-1,0));
-        //     var p2a = new Polygon2D().fromPlane(new Plane2D(-6,7,2,0));
-        //     var p2b = new Polygon2D().fromPlane(new Plane2D(0,-8,3,0));
-        //     var p3a = new Polygon2D().fromPlane(new Plane2D(2,1,9,0));
-        //     var p3b = new Polygon2D().fromPlane(new Plane2D(0,1,-2,0));
-        //     var p4 = new Polygon2D().fromPlane(new Plane2D(1,1,1,0));
+            var bounding = [
+                new Plane2D(1,0,1),
+                new Plane2D(0,1,1),
+                new Plane2D(1,0,-1),
+                new Plane2D(0,1,-1)
+            ];
+            var polygon = new Polygon2D(bounding);
 
-        //     var xplus = {a:1,b:0,c:0,d:1000000},
-        //         yplus = {a:0,b:1,c:0,d:1000000},
-        //         zplus = {a:0,b:0,c:1,d:1000000},
-        //         xmin = {a:-1,b:0,c:0,d:1000000},
-        //         ymin = {a:0,b:-1,c:0,d:1000000},
-        //         zmin = {a:0,b:0,c:-1,d:1000000};
+            var result1 = polygon.splitBy(new Plane2D(1,0,-1));
+            var result2 = polygon.splitBy(new Plane2D(1,0,1));
+            var result3 = polygon.splitBy(new Plane2D(1,0,0));
 
-        //     assert.deepEqual(p1a, {
-        //         s: {a:5, b:-2, c:1, d:0},
-        //         boundingPlanes: [yplus, zplus, ymin, zmin],
-        //     });
-        //     assert.deepEqual(p1b, {
-        //         s: {a:-6, b:2, c:-1, d:0},
-        //         boundingPlanes: [yplus, zmin, ymin, zplus],
-        //     });
+            // Coincident and same orientation
+            assert.deepEqual(result1.front, polygon);
+            assert.isUndefined(result1.back);
 
-        //     assert.deepEqual(p2a, {
-        //         s: {a:-6, b:7, c:2, d:0},
-        //         boundingPlanes: [zplus, xplus, zmin, xmin],
-        //     });
-        //     assert.deepEqual(p2b, {
-        //         s: {a:0, b:-8, c:3, d:0},
-        //         boundingPlanes: [zplus, xmin, zmin, xplus],
-        //     });
+            // Coincident and different orientation
+            console.log(result2);
+            assert.isUndefined(result2.front);
+            assert.deepEqual(result2.back, polygon);
 
-        //     assert.deepEqual(p3a, {
-        //         s: {a:2, b:1, c:9, d:0},
-        //         boundingPlanes: [xplus, yplus, xmin, ymin],
-        //     });
-        //     assert.deepEqual(p3b, {
-        //         s: {a:0, b:1, c:-2, d:0},
-        //         boundingPlanes: [xplus, ymin, xmin, yplus],
-        //     });
-
-        //     assert.deepEqual(p4, {
-        //         s: {a:1, b:1, c:1, d:0},
-        //         boundingPlanes: [yplus, zplus, ymin, zmin],
-        //     });
-
-        // });
-
-        // it('can be split by a plane', function() {
-
-        //     var s = new Plane2D(0,0,1,0);
-        //     var bounding = [
-        //         new Plane2D(1,0,0,1),
-        //         new Plane2D(0,1,0,1),
-        //         new Plane2D(-1,0,0,1),
-        //         new Plane2D(0,-1,0,1)
-        //     ];
-        //     var polygon = new Polygon2D(s, bounding);
-
-        //     var result1 = polygon.splitBy(new Plane2D(0,0,1,0));
-        //     var result2 = polygon.splitBy(new Plane2D(0,0,-1,0));
-        //     var result3 = polygon.splitBy(new Plane2D(1,0,0,0));
-
-        //     // Coincident and same orientation
-        //     assert.deepEqual(result1.front, polygon);
-        //     assert.isUndefined(result1.back);
-        //     // Coincident and different orientation
-        //     assert.isUndefined(result2.front);
-        //     assert.deepEqual(result2.back, polygon);
-        //     // Square split by ZY plane
-        //     assert.deepEqual(result3.front, {
-        //         s: {a: 0, b: 0, c: 1, d: 0},
-        //         boundingPlanes: [
-        //             {a: 1, b: 0, c: 0, d: 1},
-        //             {a: 0, b: 1, c: 0, d: 1},
-        //             {a: 1, b: 0, c: 0, d: 0},
-        //             {a: 0, b:-1, c: 0, d: 1}
-        //         ]
-        //     });
-        // });
+            // Square split by y=0 plane
+            assert.deepEqual(result3.front, {
+                boundingPlanes: [
+                    {a: 1, b: 0, c:  1},
+                    {a: 0, b: 1, c:  1},
+                    {a: 1, b: 0, c:  0},
+                    {a: 0, b: 1, c: -1},
+                ]
+            });
+        });
 
     });
 
