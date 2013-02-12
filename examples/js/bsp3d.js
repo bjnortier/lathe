0a19a12ed50edc4005d73052c751e94ddd4d83b6
Benjamin Nortier <bjnortier@gmail.com>
2013-02-12 10:19:03 +0000

    Error case for union

--- examples/js/bsp3d.js
+++ examples/js/bsp3d.js
@@ -45,4 +45,18 @@ requirejs([
     new BSPTree3DExample(t1, t2, BSP2D.intersection);
     new BSPTree3DExample(t1, t2, BSP2D.union);
     new BSPTree3DExample(t1, t2, BSP2D.difference);
+
+    var t1 = createConvexTree(
+        ['0', '1', '2', '3'], 
+        [new Plane3D(0,0,-1,0), new Plane3D(1,-1,1,10), new Plane3D(-1,-1,1,10), new Plane3D(-1,1,1,10), new Plane3D(1,1,1,10)], 
+        worldRegion);
+    var t2 = createConvexTree(
+        ['a', 'b', 'c', 'd', 'e', 'f'],
+        [new Plane3D(0,0,-1,1), new Plane3D(0,0,1,5), new Plane3D(1,0,0,5), new Plane3D(-1,0,0,5), new Plane3D(0,1,0,5), new Plane3D(0,-1,0,5)],
+        worldRegion);
+    
+    new BSPTree3DExample(t1, t2, BSP2D.intersection);
+    new BSPTree3DExample(t1, t2, BSP2D.union);
+    new BSPTree3DExample(t1, t2, BSP2D.difference);
+
 });
