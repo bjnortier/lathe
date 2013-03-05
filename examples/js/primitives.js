c93c5044db4574f6ba1755d17d5cfd1a6b77f394
Benjamin Nortier <bjnortier@gmail.com>
2013-03-05 11:23:03 +0000

    Halfsphere primitive

--- examples/js/primitives.js
+++ examples/js/primitives.js
@@ -12,6 +12,7 @@ requirejs([
         'lib/bench',
         'lib/plane3d',
         'lib/primitives/sphere',
+        'lib/primitives/halfsphere',
         'lib/primitives/cube',
         'examples/js/primitivesexample',
     ], 
@@ -19,13 +20,18 @@ requirejs([
         Bench,
         Plane3D,
         Sphere,
+        HalfSphere,
         Cube,
         PrimitivesExample) {
 
     var p1 = Bench.time(function() { return new Cube(-10,-10,-10,10,10,10); }, 'cube');
     // var p2 = Bench.time(function() { return new Cube(-2,-2,-2,10,10,10); }, 'cube');
-    var p2 = Bench.time(function() { return new Sphere(0,0,0,5,20); }, 'sphere');
+    // new PrimitivesExample(p1, p2);
 
-    new PrimitivesExample(p1, p2);
+    // var p3 = Bench.time(function() { return new Sphere(0,0,0,5,20); }, 'sphere');
+    // new PrimitivesExample(p1, p3);
+
+    var p4 = Bench.time(function() { return new HalfSphere(0,0,0,5,3); }, 'halfsphere');
+    new PrimitivesExample(p1, p4);
 
 });
