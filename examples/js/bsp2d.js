f4948c7db189cfa58df8bbc6b8dae4a702282c06
Benjamin Nortier <bjnortier@gmail.com>
2013-03-11 17:06:38 +0000

    Blind stab

--- examples/js/bsp2d.js
+++ examples/js/bsp2d.js
@@ -39,51 +39,53 @@ requirejs([
         new Node(
             new Plane2D(0,-1,-1),
             createConvexTree([new Plane2D(1,1,10), new Plane2D(-1,0,-1)]),
-            createConvexTree([new Plane2D(1,-1,10), new Plane2D(-1,0,-1)]));
+            new Cell(false));
+            // createConvexTree([new Plane2D(1,-1,10), new Plane2D(-1,0,-1)]));
     t1.createSHPs(Line2D);
 
     var t2 = 
         new Node(
             new Plane2D(-1,0,-3),
             createConvexTree([new Plane2D(0,-1,-2), new Plane2D(1,1,12)]),
-            createConvexTree([new Plane2D(0,-1,1), new Plane2D(-1,1,5)]));
+            new Cell(false));
+            // createConvexTree([new Plane2D(0,-1,1), new Plane2D(-1,1,5)]));
 
     t2.createSHPs(Line2D);
 
     new BooleanExample(t1, t2, Line2D);
 
 
-    var t3 = createConvexTree([
-        new Plane2D(1,0,5),
-        new Plane2D(0,1,1),
-        new Plane2D(0,-1,0),
-        new Plane2D(-1,0,0),
-    ])
-    t3.createSHPs(Line2D); 
+    // var t3 = createConvexTree([
+    //     new Plane2D(1,0,5),
+    //     new Plane2D(0,1,1),
+    //     new Plane2D(0,-1,0),
+    //     new Plane2D(-1,0,0),
+    // ])
+    // t3.createSHPs(Line2D); 
 
-    var t4 = createConvexTree([
-        new Plane2D(0,-1,-1), 
-        new Plane2D(1,1,12), 
-        new Plane2D(-1,0,-1)
-    ])
-    t4.createSHPs(Line2D);
+    // var t4 = createConvexTree([
+    //     new Plane2D(0,-1,-1), 
+    //     new Plane2D(1,1,12), 
+    //     new Plane2D(-1,0,-1)
+    // ])
+    // t4.createSHPs(Line2D);
 
-    // Example for the oppositely coincident SHPs, similar to the 3D case
-    new BooleanExample(t3, t4, Line2D);
+    // // Example for the oppositely coincident SHPs, similar to the 3D case
+    // new BooleanExample(t3, t4, Line2D);
 
-    // Example of non-overlapping
-    var t5 = createConvexTree([
-        new Plane2D(0,1,0), 
-        new Plane2D(-1,-1,12), 
-        new Plane2D(1,0,-1)
-    ])
-    t5.createSHPs(Line2D); 
+    // // Example of non-overlapping
+    // var t5 = createConvexTree([
+    //     new Plane2D(0,1,0), 
+    //     new Plane2D(-1,-1,12), 
+    //     new Plane2D(1,0,-1)
+    // ])
+    // t5.createSHPs(Line2D); 
 
-    var t6 = createConvexTree([
-        new Plane2D(0,-1,-1), 
-        new Plane2D(1,1,12), 
-        new Plane2D(-1,0,-1)
-    ])
-    t6.createSHPs(Line2D);
-    new BooleanExample(t5, t6, Line2D);
+    // var t6 = createConvexTree([
+    //     new Plane2D(0,-1,-1), 
+    //     new Plane2D(1,1,12), 
+    //     new Plane2D(-1,0,-1)
+    // ])
+    // t6.createSHPs(Line2D);
+    // new BooleanExample(t5, t6, Line2D);
 });
