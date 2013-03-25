80e15019eb909a8b174c8b5f16f96e66a6299f17
Benjamin Nortier <bjnortier@Benjamins-Retina-MBP.local>
2013-03-25 11:09:41 +0000

    Fix bench

--- test/benchmark/boolean.test.js
+++ test/benchmark/boolean.test.js
@@ -36,11 +36,11 @@ define([
             console.log('\nsphere & cube:')
 
             var sphere = time(function() { 
-                return new Sphere(0,0,0,5);
+                return new Sphere({x: 0, y: 0, z: 0, r: 5});
             }, 'create sphere:\t');
      
             var cube = time(function() { 
-                return new Cube(-8,-8,-8,10,10,10);
+                return new Cube({x: -8, y: -8, z: -8, w: 10, d: 10, h: 10});
             }, 'create cube:\t');
 
             ['union', 'intersection', 'difference'].forEach(function(op) {
